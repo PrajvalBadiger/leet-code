c762be2c881c63cf00fa56c6576b5b659d8aeb6e
Prajval Baidger <er.prajvalbadiger@gmail.com>
2023-10-05 00:35:51 +0530

    longest-palindromic-substring: - modified the isPalindrome function signature. - this way we do s.substring(i, j) only when the string is a Palindrome.

--- longest-palindromic-substring.ts
+++ longest-palindromic-substring.ts
@@ -3,8 +3,8 @@ function longestPalindrome(s: string): string {
     let res = "";
     for (let i = 0; i < s.length; i++) {
         for (let j = i + 2; j <= s.length; j++) {
-            const substring = s.substring(i, j);
-            if (isPalindrome(substring)) {
+            if (isPalindrome(s, i, j)) {
+                const substring = s.substring(i, j);
                 if (len < substring.length) {
                     res = substring;
                     len = substring.length;
@@ -13,9 +13,12 @@ function longestPalindrome(s: string): string {
         }
     }
 
+    // skip checking for single char as single char are always a Palindrome
+    // if there are no Palindrome strings return the first char
     if (res === "") {
         return s[0];
     }
+
     return res;
 };
 
@@ -23,11 +26,8 @@ console.log(longestPalindrome("babad"));
 console.log(longestPalindrome("cbbd"));
 console.log(longestPalindrome("kyyrjtdplseovzwjkykrjwhxquwxsfsorjiumvxjhjmgeueafubtonhlerrgsgohfosqssmizcuqryqomsipovhhodpfyudtusjhonlqabhxfahfcjqxyckycstcqwxvicwkjeuboerkmjshfgiglceycmycadpnvoeaurqatesivajoqdilynbcihnidbizwkuaoegmytopzdmvvoewvhebqzskseeubnretjgnmyjwwgcooytfojeuzcuyhsznbcaiqpwcyusyyywqmmvqzvvceylnuwcbxybhqpvjumzomnabrjgcfaabqmiotlfojnyuolostmtacbwmwlqdfkbfikusuqtupdwdrjwqmuudbcvtpieiwteqbeyfyqejglmxofdjksqmzeugwvuniaxdrunyunnqpbnfbgqemvamaxuhjbyzqmhalrprhnindrkbopwbwsjeqrmyqipnqvjqzpjalqyfvaavyhytetllzupxjwozdfpmjhjlrnitnjgapzrakcqahaqetwllaaiadalmxgvpawqpgecojxfvcgxsbrldktufdrogkogbltcezflyctklpqrjymqzyzmtlssnavzcquytcskcnjzzrytsvawkavzboncxlhqfiofuohehaygxidxsofhmhzygklliovnwqbwwiiyarxtoihvjkdrzqsnmhdtdlpckuayhtfyirnhkrhbrwkdymjrjklonyggqnxhfvtkqxoicakzsxmgczpwhpkzcntkcwhkdkxvfnjbvjjoumczjyvdgkfukfuldolqnauvoyhoheoqvpwoisniv"));
 
-function isPalindrome(s: string): boolean {
-
-    let l = 0;
-    let r = s.length - 1;
-
+function isPalindrome(s: string, l: number, r: number): boolean {
+    r = r - 1;
     while (l < r) {
         if (s[l] !== s[r]) return false;
         l++;
@@ -35,3 +35,4 @@ function isPalindrome(s: string): boolean {
     }
     return true;
 }
+
