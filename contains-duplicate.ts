8183bd1092dc0c5b8518f001b0ce3a5031136491
Prajval Baidger <er.prajvalbadiger@gmail.com>
2023-10-08 00:26:25 +0530

    contains-duplicate using set

--- contains-duplicate.ts
+++ contains-duplicate.ts
@@ -1,3 +1,4 @@
+// using hashmap
 function containsDuplicate(nums: number[]): boolean {
     let map = new Map<number, boolean>();
     for (let idx = 0; idx < nums.length; idx++) {
@@ -8,3 +9,12 @@ function containsDuplicate(nums: number[]): boolean {
     }
     return false
 };
+
+
+//using set
+function containsDuplicate2(nums: number[]): boolean {
+    let nums_set = new Set(nums);
+
+    return nums_set.size < nums.length
+}
+
