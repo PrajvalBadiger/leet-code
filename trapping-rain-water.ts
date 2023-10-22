8137a8e729bcd150dca7e230e2f18876a52ae7d1
Prajval Baidger <er.prajvalbadiger@gmail.com>
2023-10-22 16:10:50 +0530

    trapping-rain-water using two pointers

--- trapping-rain-water.ts
+++ trapping-rain-water.ts
@@ -10,5 +10,31 @@ function trap(height: number[]): number {
     return sum;
 };
 
-console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
-console.log(trap([4, 2, 0, 3, 2, 5]))
+// two pointer
+function trap2(height: number[]): number {
+    let sum = 0;
+
+    let l = 0;
+    let r = height.length - 1;
+    let leftMax = height[l];
+    let rightMax = height[r];
+
+    while(l < r) {
+        // whichever is the least of leftMax and rightMax, calculate water traped for that index
+        if (leftMax < rightMax) {
+            l++;
+            leftMax = Math.max(leftMax, height[l]);
+            sum += leftMax - height[l]
+        } else {
+            r--;
+            rightMax = Math.max(rightMax, height[r]);
+            sum += rightMax - height[r]
+        }
+
+    }
+
+    return sum;
+};
+
+console.log(trap2([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+console.log(trap2([4, 2, 0, 3, 2, 5]))
