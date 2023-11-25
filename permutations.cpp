8117b98f6eaad79c2e2a0199a02739c64399b2da
Prajval Baidger <er.prajvalbadiger@gmail.com>
2023-11-25 21:19:41 +0530

    permutations

---
+++ permutations.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<vector<int>> permute(vector<int> &nums) {
+        vector<vector<int>> res;
+
+        dfs(nums, 0, res);
+
+        return res;
+    }
+
+  private:
+    void dfs(vector<int> nums, int start, vector<vector<int>> &res) {
+        if (start == nums.size()) {
+            res.push_back(nums);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            swap(nums[i], nums[start]);
+            dfs(nums, start + 1, res);
+            swap(nums[i], nums[start]);
+        }
+    }
+};
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> res;
+    Solution *sol = new Solution();
+
+    res = sol->permute(nums);
+
+    for (auto arr : res) {
+        for (auto val : arr) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
